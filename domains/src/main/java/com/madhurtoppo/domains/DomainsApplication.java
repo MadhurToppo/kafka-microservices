1569a8b82480fa1e57664095ffefeca905566f86
Madhur Toppo <madhur.toppo@gmail.com>
2023-04-20 14:06:09 +0530

    Add 4 microservices to project

---
+++ domains/src/main/java/com/madhurtoppo/domains/DomainsApplication.java
@@ -0,0 +1,13 @@
+package com.madhurtoppo.domains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainsApplication.class, args);
+	}
+
+}
---
+++ domains/src/test/java/com/madhurtoppo/domains/DomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madhurtoppo.domains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
